d6758f6d920d288898081acc3bff057c00a77bab
wangsh <wugcelama19@gmail.com>
2021-06-11 13:31:49 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var myFriend = "5150";
+
+var myString = "Two Hundred Thirty";
+
+const myFirstNumber = (Number(myFriend));
+
+console.log(myFirstNumber);
+
+const mySecondNumber = (Number(myString));
+
+
+//It's not convert string to number because it's word.
+console.log(mySecondNumber);
+
+
+
+
